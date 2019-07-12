f85444580b274c2e3b6deccb5c9c3b6ea4451843
zhanghong <1539939746.@qq.com>
2019-07-12 14:06:45 +0800

    引入 nacos config

---
+++ sunny-spring-cloud-gateway/src/main/java/com/sunny/spring/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,26 @@
+package com.sunny.spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @描述:
+ * @版权: Copyright (c) 2019
+ * @公司:
+ * @author: zhanghong
+ * @版本: 1.0
+ * @创建日期: 2019-07-12
+ * @创建时间: 11:40
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
---
+++ sunny-spring-cloud-gateway/src/main/java/com/sunny/spring/cloud/gateway/filters/AuthFilter.java
@@ -0,0 +1,66 @@
+package com.sunny.spring.cloud.gateway.filters;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+/**
+ * @描述:
+ * @版权: Copyright (c) 2019
+ * @公司:
+ * @author: zhanghong
+ * @版本: 1.0
+ * @创建日期: 2019-07-12
+ * @创建时间: 13:39
+ **/
+@Component
+public class AuthFilter implements GlobalFilter,Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getQueryParams().getFirst("token");
+
+        if (token == null || token.isEmpty()) {
+            ServerHttpResponse response = exchange.getResponse();
+
+            // 封装错误信息
+            Map<String, Object> responseData = Maps.newHashMap();
+            responseData.put("code", 401);
+            responseData.put("message", "非法请求");
+            responseData.put("cause", "Token is empty");
+
+            try {
+                // 将信息转换为 JSON
+                ObjectMapper objectMapper = new ObjectMapper();
+                byte[] data = objectMapper.writeValueAsBytes(responseData);
+
+                // 输出错误信息到页面
+                DataBuffer buffer = response.bufferFactory().wrap(data);
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+                return response.writeWith(Mono.just(buffer));
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+}
