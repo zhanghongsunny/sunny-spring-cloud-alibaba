b533f960b5dc9e6c794e13e503fc3f441d2d890c
zhanghong <1539939746.@qq.com>
2019-07-12 14:00:45 +0800

    引入 nacos config

--- sunny-spring-cloud-alibaba-nacos-provider/src/main/java/com.sunny.spring.cloud.alibaba.nacos.provider/controller/EchoController.java
+++ sunny-spring-cloud-alibaba-nacos-provider/src/main/java/com.sunny.spring.cloud.alibaba.nacos.provider/controller/EchoController.java
@@ -1,6 +1,8 @@
 package com.sunny.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +22,24 @@ public class EchoController {
     @Value("${server.port}")
     private String port;
 
+    /**
+     * 注入配置文件上下文
+      */
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
     @GetMapping(value = "/echo/{message}")
     public String echo(@PathVariable(value = "message") String message) {
         return "Hello Nacos Discovery " + message + " i am from port " + port;
+    }
+
+
+    /**
+     *  从上下文中读取配置
+      */
+    @GetMapping(value = "/hi")
+    public String sayHi() {
+        return "Hello " + applicationContext.getEnvironment().getProperty("user.name") +  " i am from port " + port;
     }
 
 }
